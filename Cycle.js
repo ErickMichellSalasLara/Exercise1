48060fb7e3e8cb2a7ce0a069695e4dc7637b62ea
ErickMichellSalasLara <st4382@utr.edu.mx>
2026-03-02 11:11:27 -0600

    Hola profe aqui esat el trabajo si

---
+++ Cycle.js
@@ -0,0 +1,14 @@
+// What a roll, in this code we are going to do a little array using a while loop, get started
+// First, we need to create an empty array, and a variable to count the number of rolls (The last one call "Contador")
+// Second, we have to create the while loop and insert the array with the condition selected
+// Third, run the code with the array.push that idk what this code do.
+
+let arreglo = [];
+let Contador = 0;
+
+while(Contador <= 21){
+    arreglo.push(1);
+    Contador++;
+    console.log(arreglo)
+}
+    
